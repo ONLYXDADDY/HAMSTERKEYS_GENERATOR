6ae9f9c6836728521432cac9ae5ad91c0f666fcd
𝘋𝘈𝘋𝘋𝘠 <khaled.zaarei@gmail.com>
2024-08-18 17:37:58 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     
-    const MAX_KEYS_PER_GAME_PER_DAY = 12;
+    const MAX_KEYS_PER_GAME_PER_DAY = 13;
     //const EVENTS_DELAY = 20000;
 
     const games = {
